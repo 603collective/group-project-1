bc13033240bf83b42a512eb7d315606e327dc90e
mendelism <hgustafson17@gmail.com>
2022-05-05 16:58:18 -0400

    added event handler for form submission and function to display yelp data

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+// DOM Element Variables
+const searchBtn = document.querySelector("#search-btn");
+const formPark = document.querySelector("#search-park");
+const formHotels = document.querySelector("#formHotels");
+const formActivities = document.querySelector("#formActivities");
+const formRestaurants = document.querySelector("#formRestaurants");
+const resultsContainer = document.querySelector("#results");
+const hotelContainer = document.querySelector("#hotel-results");
+const activityContainer = document.querySelector("#activity-results");
+const restaurantContainer = document.querySelector("#restaurant-results");
+
 let parkNames =[];
 let activityOptions={};
 let npsData = null;
@@ -89,11 +100,22 @@ const yelpOptions = {
   }
 };
 
-function findAmenity(latitude, longitude, amenityType) {
+// Create empty array to which an object for each returned restaurant will be pushed
+let hotelArray = [];
+let foodArray = [];
+
+function findAmenity(latitude, longitude, amenity) {
   // Create URL from latitude and longitude data from NPS API
   // amenity should be "restaurants" for food and "hotels,campgrounds,bedbreakfast" for hotels.
   // Search radius is currently hardcoded to 20000 meters
   // Must opt-in to https://cors-anywhere.herokuapp.com/corsdemo for functionality
+  let amenityType;
+  // Set search query term for amenity type
+  if (amenity == "hotels") {
+    amenityType = "hotels,campgrounds,bedbreakfast";
+  } else if (amenity == "restaurants") {
+    amenityType = "restaurants"
+  };
   let yelpAPIURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=" + amenityType + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=20000"
   fetch(yelpAPIURL, yelpOptions)
   .then(function(response) {
@@ -103,34 +125,91 @@ function findAmenity(latitude, longitude, amenityType) {
   })
   .then(function(data) {
     if (data) {
-      // create empty array to which an object for each returned restaurant will be pushed
-      let amenityArray = [];
       for (let i = 0; i < data.businesses.length; i++) {
         // create array of restaurant category titles
         let amenityCat = [];
         for (let j = 0; j < data.businesses[i].categories.length; j++) {
           amenityCat.push(data.businesses[i].categories[j].title);
         };
-        amenityArray.push({
+        if (amenityType == "restaurants") { // push to empty restaurant array
+          foodArray.push({
+            "name": data.businesses[i].name,
+            "image": data.businesses[i].image_url,
+            "price": data.businesses[i].price,
+            "distance": Math.round((data.businesses[i].distance/1609)*100)/100 + " miles", //converts to miles
+            "rating": data.businesses[i].rating,
+            "categories": amenityCat});
+        } else { // push to empty hotel array
+          hotelArray.push({
             "name": data.businesses[i].name,
             "image": data.businesses[i].image_url,
             "price": data.businesses[i].price,
             "distance": Math.round((data.businesses[i].distance/1609)*100)/100 + " miles",
             "rating": data.businesses[i].rating,
             "categories": amenityCat});
+        }
       }
-    console.log(amenityArray); // array of restaurant objects containing their name, stock image, price range, and distance in miles (from meters)
-    // TO DO: execute a function that will display this data
+    }
+  })
+  .then(function() {
+    if (amenity == "hotels") {
+      displayAmenities(hotelArray, hotelContainer);
+    } else if (amenity == "restaurants") {
+      displayAmenities(foodArray, restaurantContainer);
     }
   })
 };
 
-function whichAmenities(latitude, longitude, hotels, restaurants) {
-// hotels and restaurants should be boolean
-  if (hotels) {
-    findAmenity(latitude, longitude, "hotels,campgrounds,bedbreakfast");
-  }
-  if (restaurants) {
-    findAmenity(latitude, longitude, "restaurants");
+function displayAmenities(array, container) {
+  for (let i = 0; i < array.length; i++) {
+    let nameEl = document.createElement("h3");
+    nameEl.textContent = array[i].name;
+    let imgEl = document.createElement("img");
+    imgEl.setAttribute("src", array[i].image);
+    imgEl.style.maxWidth = "300px";
+    imgEl.textContent = array[i].image;
+    let priceEl = document.createElement("p");
+    priceEl.textContent = array[i].price;
+    let distanceEl = document.createElement("p");
+    distanceEl.textContent = array[i].distance;
+    let ratingEl = document.createElement("p");
+    ratingEl.textContent = array[i].rating;
+    let catEl = document.createElement("p");
+    for (let j = 0; j < array[i].categories.length - 1; j++) {  //print each category separated by commas
+      catEl.textContent += array[i].categories[j] + ", ";
+    };
+    catEl.textContent += array[i].categories.pop(); //print last category with no comma
+    container.append(nameEl);
+    container.append(imgEl);
+    container.append(priceEl);
+    container.append(distanceEl);
+    container.append(ratingEl);
+    container.append(catEl);
   }
 };
+
+function whichAmenities(latitude, longitude, hotels, restaurants) {
+// hotels and restaurants should be boolean
+// the booleans will come from whether the boxes are checked on the form
+  if (hotels) {
+    findAmenity(latitude, longitude, "hotels");
+  };
+  if (restaurants) {
+    findAmenity(latitude, longitude, "restaurants");
+  };
+};
+
+// 
+// FORM SUBMISSION EVENT HANDLING
+// 
+
+const searchHandler = function(event) {
+  event.preventDefault();
+  // makes container viewable
+  resultsContainer.style.display = "block";
+  //Display park name in results, maybe with description?
+  //Retrieve lat/long from formPark.value
+  whichAmenities("44.409286", "-68.247501", formHotels.checked, formRestaurants.checked);
+}
+
+searchBtn.addEventListener("click", searchHandler);
